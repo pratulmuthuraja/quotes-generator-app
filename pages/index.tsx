5a9c41dc452663fe92cb467313d9cb594663f58e
Pratul Muthuraja <pratulmuthuraja@gmail.com>
2023-05-23 14:19:13 -0500

    Add gradient background container using styledcomponent

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { GradientBackgroundCon } from "@/components/QuoteGenerator/QuoteGeneratorElements";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <GradientBackgroundCon></GradientBackgroundCon>
       <main className={styles.main}></main>
     </>
   );
